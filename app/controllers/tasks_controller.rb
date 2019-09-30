1dba7dd68920a3379a821bc47c0dfca1381b42f4
kosuke.kimura <kosuke.kimura@techacademy.jp>
2019-09-30 11:20:36 +0000

    task2

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,6 +5,10 @@ class TasksController < ApplicationController
     
     def index
         @tasks = Task.order(id: :desc).page(params[:page]).per(25)
+        if logged_in?
+            @task = current_user.tasks.build  
+            @tasks = current_user.tasks.order(id: :desc).page(params[:page])
+        end
     end
 
     def show
