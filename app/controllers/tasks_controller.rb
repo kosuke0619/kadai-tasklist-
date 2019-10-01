69c09bc083bd5b0b61746b098ce15b3e1218acc2
kosuke.kimura <kosuke.kimura@techacademy.jp>
2019-10-01 12:00:45 +0000

    ee

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,9 +3,9 @@ class TasksController < ApplicationController
     before_action :correct_user, only: [:show, :edit, :update, :destroy]
     
     def index
-        @tasks = Task.order(id: :desc).page(params[:page]).per(25)
         if logged_in?
             @task = current_user.tasks.build  
+            @tasks = current_user.tasks.order(id: :desc).page(params[:page])
         end
     end
     
